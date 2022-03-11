0b4cde1508694d6378405d30232e708b54f4d2d3
yichienlu <yichien.lu89@gmail.com>
2022-03-11 16:19:22 +0800

    deploy

---
+++ js/381.fba4cb06.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_live_week7"]=self["webpackChunkvue_live_week7"]||[]).push([[381],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return o}});var t=u(6252);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function i(e,n){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(3744);const l={},v=(0,c.Z)(l,[["render",i]]);var o=v}}]);
+//# sourceMappingURL=381.fba4cb06.js.map
